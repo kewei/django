ce0191035a897eae6265553021bbf1d5e9e2dcd8
kewei <zkw20080810@gmail.com>
2017-07-25 23:54:23 +0200

    changed the template of detail.html and vote function in views

--- keweizhang/views.py
+++ keweizhang/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Question
-from django.http import HttpResponse
-from django.http import Http404
+from .models import Question, Choice
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -21,5 +21,21 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'keweizhang/detail.html', {
+                      'question': question,
+                      'error_message': "You didnot select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('keweizhang:results',
+                                            args=(question.id,)))
+
 
